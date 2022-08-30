1c9615c98f081a5ef255f279a3f8007e7a9e8f00
Robert Paiva <robertpaiva.df@gmail.com>
2022-08-30 17:23:38 -0300

    Add aula sobre herança

---
+++ ws-eclipse/heranca/src/aplication/Program.java
@@ -0,0 +1,24 @@
+package aplication;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.BusinessAccount;
+
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+			
+		BusinessAccount account = new BusinessAccount();
+		
+		account.getBalance();
+				
+		sc.close();
+		
+	}
+
+}
