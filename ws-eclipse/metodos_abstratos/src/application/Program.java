0551adb42240e55bc022431834834aeefbf87b25
Robert Paiva <robertpaiva.df@gmail.com>
2022-09-01 13:17:40 -0300

    classes e metodos abtratos

---
+++ ws-eclipse/metodos_abstratos/src/application/Program.java
@@ -0,0 +1,56 @@
+ package application;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+import javax.tools.DocumentationTool.Location;
+
+import entities.Circle;
+import entities.Rectangle;
+import entities.Shape;
+import entities_enum.Color;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		
+		Scanner sc = new Scanner(System.in);
+						
+		System.out.println("Enter the number of shapes: ");
+		int n = sc.nextInt();
+		
+		List<Shape> list = new ArrayList<>();		
+		
+		for (int i=1; i<=n; i++) {
+			System.out.println("Rectangle or Circle (r/c)? ");
+			char o = sc.next().charAt(0);
+			System.out.println("Color (BLACK/BLUE/RED)?");
+			Color color = Color.valueOf(sc.next());
+			if(o=='c') {
+				System.out.println("Inform the radius:");
+				double radius = sc.nextDouble();
+				list.add(new Circle(color, radius));
+			}else {
+				System.out.println("Inform the width:");
+				double width = sc.nextDouble();
+				System.out.println("Inform the height:");
+				double height = sc.nextDouble();
+				list.add(new Rectangle(color, width, height));
+			}
+			
+		}
+		
+		//print the result here
+		System.out.println("Shape Areas:");
+		for(Shape shape: list) {
+			System.out.println(shape.getColor().toString() + " - " + shape.area());			
+		}
+		
+		
+		sc.close();
+	}
+
+}
